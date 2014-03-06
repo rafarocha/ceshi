3d40f39b9bd6991926872dbc796bf312ea88be3a
rafa.rocha <faelcavalcanti@gmail.com>
2014-03-06 17:28:38 -0300

    melhorando exemplos com parameter

--- src/test/java/br/edu/curso/teste/experimentos/junit/parameters/FibonacciTestMelhorado.java
+++ src/test/java/br/edu/curso/teste/experimentos/junit/parameters/FibonacciTestMelhorado.java
@@ -3,36 +3,33 @@ package br.edu.curso.teste.experimentos.junit.parameters;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
-import java.util.Collection;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
-public class FibonacciTest {
-	
-	@Parameters
-	public static Collection<Object[]> data() {
+public class FibonacciTestMelhorado {
+
+	// não funciona string abaixo com parenteses
+	@Parameters(name = "{index}: fib<{0}> = {1}")
+	public static Iterable<Object[]> data() {
 		return Arrays.asList(new Object[][] {
 					{ 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 }, 
 					{ 4, 3 }, { 5, 5 }, { 6, 8 }, { 7, 13 }
 				}
 			);
 	}
-	
-	private int fInput;
-	private int fExpected;
 
-	public FibonacciTest(int input, int expected) {
-		fInput = input;
-		fExpected = expected;
-	}
+	/* NOT private */
+	@Parameter public int fInput; // first data value (0) is default
+    @Parameter(value = 1) public int fExpected;
 
 	@Test
-	public void allValues() {
+	public void all() {
 		int result = Fibonacci.compute(fInput);
 		assertEquals(fExpected, Fibonacci.compute(fInput));
 		
