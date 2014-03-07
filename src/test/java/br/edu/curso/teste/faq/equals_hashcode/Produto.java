03cfb1a9f5b4f5eb18fbee9ffc5067afa27ff5b4
rafa.rocha <faelcavalcanti@gmail.com>
2014-03-07 18:54:50 -0300

    #faq como testar equals e hashcode

---
+++ src/test/java/br/edu/curso/teste/faq/equals_hashcode/Produto.java
@@ -0,0 +1,15 @@
+package br.edu.curso.teste.faq.equals_hashcode;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.Accessors;
+import lombok.experimental.Builder;
+
+@Builder
+@Accessors(fluent = true)
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+public class Produto {
+	
+	public int codigo;
+
+}
