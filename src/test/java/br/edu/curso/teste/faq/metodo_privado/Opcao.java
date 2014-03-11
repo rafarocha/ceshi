0d57a31d7d50a7d74a4f3dec8400de7a8b96a304
rafa.rocha <faelcavalcanti@gmail.com>
2014-03-11 11:47:40 -0300

    exemplo e exercício para método privado #faq

---
+++ src/test/java/br/edu/curso/teste/faq/metodo_privado/Opcao.java
@@ -0,0 +1,40 @@
+package br.edu.curso.teste.faq.metodo_privado;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.Accessors;
+import lombok.experimental.Builder;
+
+@Builder
+@Accessors(fluent = true)
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+public class Opcao {
+	
+	public int porta;
+	public int janela;
+	public int chave;
+	public int anel;
+	public int codigo;
+	
+	public int janela() {
+		return porta + janela; 
+	}
+	
+	public int chave() {
+		return janela() + chave;
+	}
+	
+	public int anel() {
+		return chave() + anel;
+	}
+	
+	public int codigo() {
+		return anel() + codigo;
+	}
+	
+	public void validar() {
+		if ( codigo() == 0 || codigo() > 100 )
+			throw new IllegalStateException("Não pode ser zero ou maior que 100");
+	}
+
+}
